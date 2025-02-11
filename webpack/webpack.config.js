5c055d1807dc2656f1c9dd898a34b9564cea23b3
Sanket P Bhamre <bhamresanket@gmail.com>
2025-02-11 09:58:19 +0530

    Delete webpack directory

--- webpack/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.common.js");
-
-module.exports = () => {
-  const envConfig = require(`./webpack.${process.env.NODE_ENV || "dev"}.js`);
-  return merge(commonConfig, envConfig);
-};
